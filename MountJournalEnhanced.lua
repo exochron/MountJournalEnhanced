dad946ec621a5b8b1f97fc34b8f146842e444f8d
exochron <exochron@gamerstate.de>
2018-08-19 15:09:43 +0200

    - fix SetIsFavorite

--- MountJournalEnhanced.lua
+++ MountJournalEnhanced.lua
@@ -59,74 +59,27 @@ local function C_MountJournal_GetDisplayedMountInfoExtra(index)
     return C_MountJournal.GetMountInfoExtraByID(mountId)
 end
 
-local function C_MountJournal_SetIsFavorite(oldIndex, isFavorited)
-    local mappedOldIndex = ADDON:MapIndex(oldIndex)
-    local _, searchSpellId = ADDON.hooks["GetDisplayedMountInfo"](mappedOldIndex)
-    local result = ADDON.hooks["SetIsFavorite"](mappedOldIndex, isFavorited)
-
-    table.remove(ADDON.indexMap, oldIndex)
-
-    local beginnLoop, endLoop = 1, mappedOldIndex
-    if not isFavorited then
-        beginnLoop = mappedOldIndex
-        endLoop = ADDON.hooks["GetNumDisplayedMounts"]()
+local function CallHookedFunction(functionName, index, arg1, arg2)
+    local mappedIndex = ADDON:MapIndex(index)
+    if nil~= mappedIndex then
+        return ADDON.hooks[functionName](mappedIndex, arg1, arg2)
     end
-
-    local newPosition
-    for i = beginnLoop, endLoop do
-        local _, spellId = ADDON.hooks["GetDisplayedMountInfo"](i)
-        if searchSpellId == spellId then
-            newPosition = i
-            break
-        end
-    end
-
-    if isFavorited then
-        for i = 1, oldIndex - 1 do
-            if ADDON.indexMap[i] >= newPosition then
-                ADDON.indexMap[i] = ADDON.indexMap[i] + 1
-            end
-        end
-
-        table.insert(ADDON.indexMap, newPosition)
-        table.sort(ADDON.indexMap)
-    elseif ADDON.settings.filter.onlyFavorites then
-        for i = oldIndex, #ADDON.indexMap do
-            ADDON.indexMap[i] = ADDON.indexMap[i] - 1
-        end
-    else
-        for i = oldIndex, #ADDON.indexMap do
-            local position = ADDON.indexMap[i]
-            if position <= newPosition then
-                ADDON.indexMap[i] = position - 1
-            else
-                break
-            end
-        end
-
-        table.insert(ADDON.indexMap, newPosition)
-        table.sort(ADDON.indexMap)
-    end
-
-    return result
 end
 
 local function RegisterMountJournalHooks()
     ADDON:Hook(C_MountJournal, "GetNumDisplayedMounts", C_MountJournal_GetNumDisplayedMounts)
     ADDON:Hook(C_MountJournal, "GetDisplayedMountInfo", C_MountJournal_GetDisplayedMountInfo)
     ADDON:Hook(C_MountJournal, "GetDisplayedMountInfoExtra", C_MountJournal_GetDisplayedMountInfoExtra)
-    ADDON:Hook(C_MountJournal, "SetIsFavorite", C_MountJournal_SetIsFavorite)
+    ADDON:Hook(C_MountJournal, "SetIsFavorite", function(index, isFavored)
+        local result = CallHookedFunction("SetIsFavorite", index, isFavored)
+        ADDON:UpdateIndexMap()
+        return result
+    end)
     ADDON:Hook(C_MountJournal, "GetIsFavorite", function(index)
-        local mappedIndex = ADDON:MapIndex(index)
-        if nil~= mappedIndex then
-            return ADDON.hooks["GetIsFavorite"](mappedIndex)
-        end
+        return CallHookedFunction("GetIsFavorite", index)
     end)
     ADDON:Hook(C_MountJournal, "Pickup", function(index)
-        local mappedIndex = ADDON:MapIndex(index)
-        if nil~= mappedIndex then
-            return ADDON.hooks["Pickup"](mappedIndex)
-        end
+        return CallHookedFunction("Pickup", index)
     end)
 end
 
