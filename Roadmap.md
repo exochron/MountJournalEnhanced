62bf4e4cec4c2be1858f65e40fec8fc2bc44ab28
exochron <exochron@freenet.de>
2021-07-04 09:44:28 +0200

    - UI: added button to switch through color variations if the mount has that (eg. Walking Ancient, some class mounts)

--- Roadmap.md
+++ Roadmap.md
@@ -1,13 +1,13 @@
 
 ### Next
 - special summon animation
-- switch through mount variation (eg. High Priest's Lightsworn Seeker: C_MountJournal.GetMountAllCreatureDisplayInfoByID(861))
+- activate sound effects (with global setting)
 
 ### More Ideas:
 - more animation control in display (start/pause, movements, with sound effects)
 - color filter
 - variation filter (undead, mech, elemental, magic...)
-- option: auto favorite new mounts; even those learned during offline time/on alts
+- proper favorite profiles with one global profile
 - favorite groups / custom filter groups
 - favorite menu at SummonRandomFavoriteButton?
 - link to wowhead (?)
--- UI/DisplayDirector.lua
+++ UI/DisplayDirector.lua
@@ -1,4 +1,4 @@
-local ADDON_NAME, ADDON = ...
+local _, ADDON = ...
 
 -- MountJournal.MountDisplay.ModelScene:GetActorByTag("unwrapped"):SetAnimation(X)
 --0=idle
@@ -19,6 +19,21 @@ local ADDON_NAME, ADDON = ...
 --636=mountspecial
 
 local helpTooltip
+local buttons = {}
+local container
+
+local function UpdateContainer()
+    local activeButtons = 0
+
+    for _, button in ipairs(buttons) do
+        if button:IsShown() then
+            button:SetPoint("LEFT", 2 + (activeButtons * 18), 0)
+            activeButtons = activeButtons + 1
+        end
+    end
+
+    container:SetSize((activeButtons * 18) + 4, 23)
+end
 
 local function HideOriginalElements()
     local scene = MountJournal.MountDisplay.ModelScene
@@ -35,12 +50,11 @@ local function SetupModelActor()
             actor:StopAnimationKit()
         end
     end
-    ADDON.Events:RegisterCallback("OnUpdateMountDisplay", callback, ADDON_NAME .. 'DisplayDirector')
+    ADDON.Events:RegisterCallback("OnUpdateMountDisplay", callback, 'DisplayDirector')
 end
 
-local function BuildControlContainer(width)
+local function BuildControlContainer()
     local frame = CreateFrame("Frame", nil, MountJournal.MountDisplay)
-    frame:SetSize(width, 23)
     frame:SetAlpha(0.5)
     frame:Hide()
     local rightTexture = frame:CreateTexture(nil, "BACKGROUND")
@@ -63,17 +77,11 @@ local function BuildControlContainer(width)
     return frame
 end
 
-local function InitButton(button, frame, relativeTo, tooltip, tooltipText)
-    if relativeTo then
-        button:SetPoint("LEFT", relativeTo, "RIGHT", 0, 0)
-    else
-        button:SetPoint("LEFT", 2, 0)
-    end
-
+local function InitButton(button, tooltip, tooltipText)
     -- overwrite default tooltip handling (which might cause taint)
     button:SetScript("OnEnter", function()
-        frame:SetAlpha(1)
-        frame:Show()
+        container:SetAlpha(1)
+        container:Show()
         helpTooltip:SetOwner(button, "ANCHOR_BOTTOM")
         helpTooltip:SetText(tooltip, HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
         if tooltipText then
@@ -82,14 +90,16 @@ local function InitButton(button, frame, relativeTo, tooltip, tooltipText)
         helpTooltip:Show()
     end)
     button:SetScript("OnLeave", function()
-        frame:SetAlpha(0.5)
+        container:SetAlpha(0.5)
         helpTooltip:Hide()
     end)
+
+    table.insert(buttons, button)
 end
 
-local function BuildButton(frame, relativeTo, tooltip, tooltipText)
-    local button = CreateFrame("Button", nil, frame, "ModelControlButtonTemplate")
-    InitButton(button, frame, relativeTo, tooltip, tooltipText)
+local function BuildButton(tooltip, tooltipText)
+    local button = CreateFrame("Button", nil, container, "ModelControlButtonTemplate")
+    InitButton(button, tooltip, tooltipText)
 
     return button
 end
@@ -102,9 +112,9 @@ local function CheckButtonStatus(self)
     end
 end
 
-local function BuildCheckButton(frame, relativeTo, tooltip, tooltipText, OnInitShow)
-    local button = CreateFrame("CheckButton", nil, frame, "ModelControlButtonTemplate")
-    InitButton(button, frame, relativeTo, tooltip, tooltipText)
+local function BuildCheckButton(tooltip, tooltipText, OnInitShow)
+    local button = CreateFrame("CheckButton", nil, container, "ModelControlButtonTemplate")
+    InitButton(button, tooltip, tooltipText)
     button:HookScript("OnShow", OnInitShow)
     button:HookScript("OnShow", CheckButtonStatus)
     button:HookScript("OnClick", CheckButtonStatus)
@@ -112,10 +122,10 @@ local function BuildCheckButton(frame, relativeTo, tooltip, tooltipText, OnInitS
     return button
 end
 
-local function BuildCameraButton(frame, relativeTo, tooltip, tooltipText, cameraMode, amountPerSceond)
-    local button = CreateFrame("Button", nil, frame, "ModelControlButtonTemplate,ModifyOrbitCameraBaseButtonTemplate")
+local function BuildCameraButton(tooltip, tooltipText, cameraMode, amountPerSceond)
+    local button = CreateFrame("Button", nil, container, "ModelControlButtonTemplate,ModifyOrbitCameraBaseButtonTemplate")
     button:SetSize(18, 18)
-    InitButton(button, frame, relativeTo, tooltip, tooltipText)
+    InitButton(button, tooltip, tooltipText)
 
     button.cameraMode = cameraMode
     button.amountPerSecond = amountPerSceond
@@ -132,20 +142,20 @@ end
 local function BuildCameraPanel()
     local L = ADDON.L
 
-    local frame = BuildControlContainer(166)
-    frame:SetPoint("BOTTOM", 0, 15)
+    container = BuildControlContainer()
+    container:SetPoint("BOTTOM", 0, 15)
 
-    helpTooltip = CreateFrame("GameTooltip", "MJEDisplayHelpToolTip", frame, "SharedNoHeaderTooltipTemplate")
+    helpTooltip = CreateFrame("GameTooltip", "MJEDisplayHelpToolTip", container, "SharedNoHeaderTooltipTemplate")
 
     local scene = MountJournal.MountDisplay.ModelScene
     scene:HookScript("OnEnter", function()
-        frame:Show()
+        container:Show()
     end)
     scene:HookScript("OnLeave", function()
-        frame:Hide()
+        container:Hide()
     end)
 
-    local special = BuildButton(frame, nil, "/mountspecial")
+    local special = BuildButton("/mountspecial")
     special.icon:SetTexture("Interface/GossipFrame/CampaignGossipIcons") -- from atlas: campaignavailablequesticon
     special.icon:SetTexCoord(0.1875, 0.421875, 0.37, 0.85)
     special:HookScript("OnClick", function()
@@ -157,7 +167,7 @@ local function BuildCameraPanel()
         end
     end)
 
-    local char = BuildCheckButton(frame, special, MOUNT_JOURNAL_PLAYER, nil, PlayerPreviewToggle.OnShow)
+    local char = BuildCheckButton(MOUNT_JOURNAL_PLAYER, nil, PlayerPreviewToggle.OnShow)
     char.icon:SetTexture(386865) -- interface/friendsframe/ui-toast-toasticons.blp
     char.icon:SetTexCoord(0.04, 0.58, 0.04, 0.92, 0.21, 0.58, 0.21, 0.92)
     char.icon:SetVertexColor(1, 0.7, 0.1)
@@ -170,27 +180,52 @@ local function BuildCameraPanel()
         ADDON.UI:UpdateMountDisplay(true)
     end)
 
-    local zoomIn = BuildCameraButton(frame, char, ZOOM_IN, KEY_MOUSEWHEELUP, ORBIT_CAMERA_MOUSE_MODE_ZOOM, 0.9)
+    local color = BuildButton(L.TOGGLE_COLOR)
+    color.icon:SetTexture("Interface/OptionsFrame/ColorblindSettings") -- from atlas: colorblind-colorwheel
+    color.icon:SetTexCoord(0.00390625, 0.582031, 0.00390625, 0.582031)
+    color.icon:SetSize(10, 10)
+    color:HookScript("OnClick", function()
+        local mountID, mountVariation = ADDON.Api:GetSelected()
+        local creatureData = C_MountJournal.GetMountAllCreatureDisplayInfoByID(mountID)
+        if nil == mountVariation then
+            local creatureDisplayID = C_MountJournal.GetMountInfoExtraByID(mountID)
+            for i, d in ipairs(creatureData) do
+                if d.creatureDisplayID == creatureDisplayID then
+                    mountVariation = i
+                    break
+                end
+            end
+        end
+        mountVariation = mountVariation or 1
+
+        if #creatureData >= mountVariation + 1 then
+            ADDON.Api:SetSelected(mountID, mountVariation + 1)
+        else
+            ADDON.Api:SetSelected(mountID, 1)
+        end
+    end)
+
+    local zoomIn = BuildCameraButton(ZOOM_IN, KEY_MOUSEWHEELUP, ORBIT_CAMERA_MOUSE_MODE_ZOOM, 0.9)
     zoomIn.icon:SetTexCoord(0.57812500, 0.82812500, 0.14843750, 0.27343750)
 
-    local zoomOut = BuildCameraButton(frame, zoomIn, ZOOM_OUT, KEY_MOUSEWHEELDOWN, ORBIT_CAMERA_MOUSE_MODE_ZOOM, -0.9)
+    local zoomOut = BuildCameraButton(ZOOM_OUT, KEY_MOUSEWHEELDOWN, ORBIT_CAMERA_MOUSE_MODE_ZOOM, -0.9)
     zoomOut.icon:SetTexCoord(0.29687500, 0.54687500, 0.00781250, 0.13281250)
 
-    local rotateLeft = BuildCameraButton(frame, zoomOut, ROTATE_LEFT, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_YAW_ROTATION, -3)
+    local rotateLeft = BuildCameraButton(ROTATE_LEFT, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_YAW_ROTATION, -3)
     rotateLeft.icon:SetTexCoord(0.01562500, 0.26562500, 0.28906250, 0.41406250)
 
-    local rotateRight = BuildCameraButton(frame, rotateLeft, ROTATE_RIGHT, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_YAW_ROTATION, 3)
+    local rotateRight = BuildCameraButton(ROTATE_RIGHT, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_YAW_ROTATION, 3)
     rotateRight.icon:SetTexCoord(0.57812500, 0.82812500, 0.28906250, 0.41406250)
 
-    local rotateUp = BuildCameraButton(frame, rotateRight, L.ROTATE_UP, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_PITCH_ROTATION, -3)
+    local rotateUp = BuildCameraButton(L.ROTATE_UP, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_PITCH_ROTATION, -3)
     rotateUp.icon:SetTexCoord(0.01562500, 0.26562500, 0.28906250, 0.41406250)
     rotateUp.icon:SetRotation(-90)
 
-    local rotateDown = BuildCameraButton(frame, rotateUp, L.ROTATE_DOWN, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_PITCH_ROTATION, 3)
+    local rotateDown = BuildCameraButton(L.ROTATE_DOWN, ROTATE_TOOLTIP, ORBIT_CAMERA_MOUSE_MODE_PITCH_ROTATION, 3)
     rotateDown.icon:SetTexCoord(0.57812500, 0.82812500, 0.28906250, 0.41406250)
     rotateDown.icon:SetRotation(-90)
 
-    local reset = BuildButton(frame, rotateDown, RESET_POSITION)
+    local reset = BuildButton(RESET_POSITION)
     reset:HookScript("OnClick", function()
         local cam = MountJournal.MountDisplay.ModelScene:GetActiveCamera()
         if cam then
@@ -200,10 +235,22 @@ local function BuildCameraPanel()
             cam:SetZoomDistance(info.zoomDistance);
         end
     end)
+
+    UpdateContainer()
 end
 
 ADDON.Events:RegisterCallback("loadUI", function()
     HideOriginalElements()
     SetupModelActor()
     BuildCameraPanel()
+end, "display director")
+
+ADDON.Events:RegisterCallback("OnUpdateMountDisplay", function()
+    local mountID = ADDON.Api:GetSelected()
+    local creatureData = C_MountJournal.GetMountAllCreatureDisplayInfoByID(mountID)
+    local _, _, _, isSelfMount = C_MountJournal.GetMountInfoExtraByID(mountID);
+    buttons[2]:SetShown(not isSelfMount) -- char toggle
+    buttons[3]:SetShown(#creatureData > 1) -- color toggle
+
+    UpdateContainer()
 end, "display director")
