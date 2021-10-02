596475f39d5e4b98c949cd37a3a0199c780c143e
exochron <exochron@freenet.de>
2021-10-02 15:50:52 +0200

    - UI: added resize button in bottom right corner

--- SlashCommand.lua
+++ SlashCommand.lua
@@ -1,15 +1,17 @@
 local ADDON_NAME, ADDON = ...
 
 SLASH_MOUNTJOURNALENHANCED1, SLASH_MOUNTJOURNALENHANCED2 = '/mountjournalenhanced', '/mje'
-function SlashCmdList.MOUNTJOURNALENHANCED(msg, editBox)
+function SlashCmdList.MOUNTJOURNALENHANCED(msg)
     msg = msg:lower()
 
-    if (msg == "debug on") then
+    if msg == "debug on" then
         ADDON.settings.ui.debugMode = true
         print("MountJournalEnhanced: Debug mode activated.")
-    elseif (msg == "debug off") then
+    elseif msg == "debug off" then
         ADDON.settings.ui.debugMode = false
         print("MountJournalEnhanced: Debug mode deactivated.")
+    elseif msg == "reset size" then
+        ADDON.UI:RestoreWindowSize()
     else
         local title = GetAddOnMetadata(ADDON_NAME, "Title")
         InterfaceOptionsFrame_OpenToCategory(title)
