071cb425927a6f99e72221101b8ae377029c972a
exochron <exochron@freenet.de>
2023-12-08 16:12:14 +0100

    feat(ui): Right-Click on compartment opens options [[gh#99](https://github.com/exochron/MountJournalEnhanced/issues/99)]

---
+++ AddonCompartment.lua
@@ -0,0 +1,25 @@
+local ADDON_NAME, ADDON = ...
+
+AddonCompartmentFrame:RegisterAddon({
+    text = C_AddOns.GetAddOnMetadata(ADDON_NAME, "Title"),
+    icon = C_AddOns.GetAddOnMetadata(ADDON_NAME, "IconTexture"),
+    registerForAnyClick = true,
+    notCheckable = true,
+    func = function(_, _, _, _, mouseButton)
+        if mouseButton == "RightButton" then
+            ADDON:OpenOptions()
+        else
+            ToggleCollectionsJournal(COLLECTIONS_JOURNAL_TAB_INDEX_MOUNTS)
+        end
+    end,
+    funcOnEnter = function(menuItem)
+        GameTooltip:SetOwner(menuItem, "ANCHOR_CURSOR")
+        GameTooltip:SetText("|T" .. C_AddOns.GetAddOnMetadata(ADDON_NAME, "IconTexture") .. ":0|t " .. C_AddOns.GetAddOnMetadata(ADDON_NAME, "Title"))
+        GameTooltip:AddLine(" ")
+        GameTooltip:AddLine(ADDON.L.COMPARTMENT_TOOLTIP)
+        GameTooltip:Show()
+    end,
+    funcOnLeave = function()
+        GameTooltip:Hide()
+    end,
+})
