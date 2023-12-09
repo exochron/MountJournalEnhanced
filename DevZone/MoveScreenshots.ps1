9f5abfa574555f4d72a97837a0ffcba3ccf5476e
exochron <exochron@freenet.de>
2023-12-09 09:48:36 +0100

    chore: updated screenshots

--- DevZone/MoveScreenshots.ps1
+++ DevZone/MoveScreenshots.ps1
@@ -1,25 +1,21 @@
-# move most recent screenshot into Images with specific name
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\11_options.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\10_options-menu.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\09_favorites-menu.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\08_notes.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\07_filter-rarity.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\06_filter-color.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\05_filter-family.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\04_filter-type.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\03_filter-source.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\02_sort.jpg -Force }
-gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last 1 | ForEach-Object { move ..\..\..\Screenshots\$_ .\Images\01_special.jpg -Force }
+# move most recent screenshot into ./Images with specific name
+$files = ("01_special.jpg", '02_sort.jpg', '03_filter-source.jpg', '04_filter-type.jpg', '05_filter-family.jpg', '06_filter-color.jpg', '07_filter-rarity.jpg', '08_notes.jpg', '09_favorites-menu.jpg', '10_options-menu.jpg', '11_options.jpg')
+gci ..\..\..\Screenshots\ | sort LastWriteTime |select -last $files.Count | ForEach-Object -Begin{ $i=0 } -Process { $f=$files[$i++]; move ..\..\..\Screenshots\$_ .\Images\$f -Force }
 
-# using imagick convert
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 01_special.jpg 01_special.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 02_sort.jpg 02_sort.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 03_filter-source.jpg 03_filter-source.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 04_filter-type.jpg 04_filter-type.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x1050+10+130 05_filter-family.jpg 05_filter-family.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 06_filter-color.jpg 06_filter-color.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 07_filter-rarity.jpg 07_filter-rarity.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 08_notes.jpg 08_notes.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 880x800+10+130 09_favorites-menu.jpg 09_favorites-menu.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 390x395+800+130 10_options-menu.jpg 10_options-menu.jpg
-docker run --rm -v .\Images\:/imgs dpokidov/imagemagick:latest -crop 870x895+1745+140 11_options.jpg 11_options.jpg
+# using imagick convert to crop images
+# https://imagemagick.org/script/command-line-options.php#crop
+function convert
+{
+    docker run --rm -d -v .\Images\:/imgs dpokidov/imagemagick:latest @args
+}
+convert -crop 880x800+10+130 01_special.jpg 01_special.jpg
+convert -crop 880x800+10+130 02_sort.jpg 02_sort.jpg
+convert -crop 880x800+10+130 03_filter-source.jpg 03_filter-source.jpg
+convert -crop 880x800+10+130 04_filter-type.jpg 04_filter-type.jpg
+convert -crop 880x1050+10+130 05_filter-family.jpg 05_filter-family.jpg
+convert -crop 880x800+10+130 06_filter-color.jpg 06_filter-color.jpg
+convert -crop 880x800+10+130 07_filter-rarity.jpg 07_filter-rarity.jpg
+convert -crop 880x800+10+130 08_notes.jpg 08_notes.jpg
+convert -crop 880x800+10+130 09_favorites-menu.jpg 09_favorites-menu.jpg
+convert -crop 415x415+800+130 10_options-menu.jpg 10_options-menu.jpg
+convert -crop 870x895+1745+140 11_options.jpg 11_options.jpg
