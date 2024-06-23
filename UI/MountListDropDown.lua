7b9ed4437f22bc6ed9e308099ae87da9c563886d
exochron <exochron@freenet.de>
2024-06-23 10:58:51 +0200

    feat(ui): use new menu api for context menu (11.0)

--- UI/MountListDropDown.lua
+++ UI/MountListDropDown.lua
@@ -1,15 +1,15 @@
 local ADDON_NAME, ADDON = ...
 
 local menu
-local menuMountId
+local currentMountId
 
-local function InitializeMountOptionsMenu(sender, level)
+local function InitializeUIDDM(sender, level)
 
-    if not menuMountId then
+    if not currentMountId then
         return
     end
 
-    local creatureName, spellId, icon, active, isUsable, sourceType, isFavorite, isFaction, faction, hideOnChar, isCollected, mountId = ADDON.Api:GetMountInfoByID(menuMountId)
+    local creatureName, spellId, icon, active, isUsable, sourceType, isFavorite, isFaction, faction, hideOnChar, isCollected, mountId = ADDON.Api:GetMountInfoByID(currentMountId)
 
     local info = { notCheckable = true }
 
@@ -26,7 +26,7 @@ local function InitializeMountOptionsMenu(sender, level)
 
     info.func = function()
         if needsFanfare then
-            ADDON.Api:SetSelected(menuMountId)
+            ADDON.Api:SetSelected(currentMountId)
         end
         MountJournalMountButton_UseMount(mountId)
     end
@@ -34,19 +34,19 @@ local function InitializeMountOptionsMenu(sender, level)
     UIDropDownMenu_AddButton(info, level)
 
     if not needsFanfare and isCollected then
-        local isFavorite, canFavorite = ADDON.Api:GetIsFavoriteByID(menuMountId)
+        local isFavorite, canFavorite = ADDON.Api:GetIsFavoriteByID(currentMountId)
         info = { notCheckable = true, disabled = not canFavorite }
 
         if isFavorite then
             info.text = BATTLE_PET_UNFAVORITE
             info.func = function()
-                ADDON.Api:SetIsFavoriteByID(menuMountId, false)
+                ADDON.Api:SetIsFavoriteByID(currentMountId, false)
                 ADDON:FilterMounts()
             end
         else
             info.text = BATTLE_PET_FAVORITE
             info.func = function()
-                ADDON.Api:SetIsFavoriteByID(menuMountId, true)
+                ADDON.Api:SetIsFavoriteByID(currentMountId, true)
                 ADDON:FilterMounts()
             end
         end
@@ -90,12 +90,80 @@ local function InitializeMountOptionsMenu(sender, level)
     UIDropDownMenu_AddButton({ text = CANCEL, notCheckable = true, }, level)
 end
 
-function ADDON.UI:HandleListDropDown(sender, anchor)
-    if menu == nil then
-        menu = CreateFrame("Frame", ADDON_NAME .. "MountOptionsMenu", MountJournal, "UIDropDownMenuTemplate")
-        UIDropDownMenu_Initialize(menu, InitializeMountOptionsMenu, "MENU")
+local function CreateContextMenu(owner, rootDescription, mountId)
+    rootDescription:SetTag("MENU_MOUNT_COLLECTION_MOUNT");
+
+    local _, spellId, _, active, isUsable, _, _, _, _, _, isCollected, menuMountID = ADDON.Api:GetMountInfoByID(mountId)
+
+    local text;
+    local checkEnabled = false;
+    local needsFanfare = C_MountJournal.NeedsFanfare(menuMountID);
+    if needsFanfare then
+        text = UNWRAP;
+    elseif active then
+        text = BINDING_NAME_DISMOUNT;
+    else
+        text = MOUNT;
+        checkEnabled = true
     end
 
-    menuMountId = sender.mountID
-    ToggleDropDownMenu(1, nil, menu, anchor, 0, 0)
+    local mountButton = rootDescription:CreateButton(text, function()
+        if needsFanfare then
+            ADDON.Api:SetSelected(menuMountID)
+        end
+        MountJournalMountButton_UseMount(menuMountID);
+    end);
+
+    if checkEnabled then
+        mountButton:SetEnabled(isUsable);
+    end
+
+    if not needsFanfare then
+        local button;
+        local isFavorite, canFavorite = ADDON.Api:GetIsFavoriteByID(menuMountID)
+        if isFavorite then
+            button = rootDescription:CreateButton(BATTLE_PET_UNFAVORITE, function()
+                ADDON.Api:SetIsFavoriteByID(menuMountID, false)
+                ADDON:FilterMounts()
+            end);
+        else
+            button = rootDescription:CreateButton(BATTLE_PET_FAVORITE, function()
+                ADDON.Api:SetIsFavoriteByID(menuMountID, true)
+                ADDON:FilterMounts()
+            end);
+        end
+        button:SetEnabled(isCollected and canFavorite)
+    end
+
+    if spellId then
+        if ADDON.settings.hiddenMounts[spellId] then
+            rootDescription:CreateButton(SHOW, function()
+                ADDON.settings.hiddenMounts[spellId] = nil
+                ADDON:FilterMounts()
+            end)
+        else
+            rootDescription:CreateButton(HIDE, function()
+                ADDON.settings.hiddenMounts[spellId] = true
+                ADDON:FilterMounts()
+            end)
+        end
+    end
+
+    rootDescription:CreateButton(SET_NOTE, function()
+        ADDON.UI:CreateNotesFrame(mountId)
+    end)
+end
+
+function ADDON.UI:HandleListDropDown(sender, anchor)
+    if MenuUtil then
+        MenuUtil.CreateContextMenu(anchor, CreateContextMenu, sender.mountID)
+    else
+        if menu == nil then
+            menu = CreateFrame("Frame", ADDON_NAME .. "MountOptionsMenu", MountJournal, "UIDropDownMenuTemplate")
+            UIDropDownMenu_Initialize(menu, InitializeUIDDM, "MENU")
+        end
+
+        currentMountId = sender.mountID
+        ToggleDropDownMenu(1, nil, menu, anchor, 0, 0)
+    end
 end
