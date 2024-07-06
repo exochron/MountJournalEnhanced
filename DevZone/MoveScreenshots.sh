4b64c595b1adbd6ba376166539d078ceff5307c8
exochron <exochron@freenet.de>
2024-07-06 10:23:46 +0200

    chore: updated Screenshots

--- DevZone/MoveScreenshots.sh
+++ DevZone/MoveScreenshots.sh
@@ -3,6 +3,8 @@
 cd Images || return
 
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 11_options.jpg
+mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 13_preview.jpg
+mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 12_skyriding-talents.jpg
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 10_options-menu.jpg
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 09_favorites-menu.jpg
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 08_notes.jpg
@@ -14,14 +16,16 @@ mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 03_filter-source.jpg
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 02_sort.jpg
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 01_special.jpg
 
-convert -crop 804x735+11+121 01_special.jpg 01_special.jpg
-convert -crop 804x735+11+121 02_sort.jpg 02_sort.jpg
-convert -crop 804x735+11+121 03_filter-source.jpg 03_filter-source.jpg
-convert -crop 804x735+11+121 04_filter-type.jpg 04_filter-type.jpg
-convert -crop 804x964+11+121 05_filter-family.jpg 05_filter-family.jpg
-convert -crop 804x735+11+121 06_filter-color.jpg 06_filter-color.jpg
-convert -crop 804x735+11+121 07_filter-rarity.jpg 07_filter-rarity.jpg
-convert -crop 804x735+11+121 08_notes.jpg 08_notes.jpg
-convert -crop 804x735+11+121 09_favorites-menu.jpg 09_favorites-menu.jpg
-convert -crop 364x373+745+128 10_options-menu.jpg 10_options-menu.jpg
-convert -crop 799x820+1644+130 11_options.jpg 11_options.jpg
+magick 01_special.jpg -crop 804x735+11+121 01_special.jpg
+magick 02_sort.jpg -crop 804x735+11+121 02_sort.jpg
+magick 03_filter-source.jpg -crop 804x735+11+121 03_filter-source.jpg
+magick 04_filter-type.jpg -crop 804x735+11+121 04_filter-type.jpg
+magick 05_filter-family.jpg -crop 804x964+11+121 05_filter-family.jpg
+magick 06_filter-color.jpg -crop 804x735+11+121 06_filter-color.jpg
+magick 07_filter-rarity.jpg -crop 804x735+11+121 07_filter-rarity.jpg
+magick 08_notes.jpg -crop 804x735+11+121 08_notes.jpg
+magick 09_favorites-menu.jpg -crop 804x735+11+121 09_favorites-menu.jpg
+magick 10_options-menu.jpg -crop 364x373+745+128 10_options-menu.jpg
+magick 11_options.jpg -crop 799x820+1644+130 11_options.jpg
+magick 12_skyriding-talents.jpg -crop 750x350+10+110 12_skyriding-talents.jpg
+magick 13_preview.jpg -crop 519x634+11+121 13_preview.jpg
