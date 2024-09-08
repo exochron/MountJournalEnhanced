749ac7cf2ac2bca1e5a9a57fa74135a3663507db
exochron <exochron@freenet.de>
2024-09-08 09:22:44 +0200

    fix(display): replace achievement links more dynamically

--- UI/DisplayText.lua
+++ UI/DisplayText.lua
@@ -1,16 +1,18 @@
 local _, ADDON = ...
 
-local function updateSourceText(search, replace, sourceText)
-    local text, count = sourceText:gsub(search, replace, 1)
+local function updateSourceText(replace, sourceText)
+    local text, count = sourceText:gsub(":%s?|r%s?.+|n", ":|r "..replace.."|n", 1)
+    if count == 0 then
+        text = sourceText:gsub(":%s?|r%s?.+$", ":|r "..replace, 1)
+    end
     MountJournal.MountDisplay.InfoButton.Source:SetText(text)
-    return count
 end
 
 local function replaceTextWithLinks()
     local mountId = ADDON.Api:GetSelected()
-    local _, spellId = C_MountJournal.GetMountInfoByID(mountId)
+    local _, spellId, _, _, _, sourceType = C_MountJournal.GetMountInfoByID(mountId)
 
-    local achievementIds = ADDON.DB.Source.Achievement[spellId]
+    local achievementIds = ADDON.DB.Source.Achievement[spellId] or ADDON.DB.FeatsOfStrength[mountId]
     if achievementIds and achievementIds ~= true then
         if type(achievementIds)=="number" then
             achievementIds = {achievementIds}
@@ -18,13 +20,20 @@ local function replaceTextWithLinks()
         local _, _, sourceText = C_MountJournal.GetMountInfoExtraByID(mountId)
 
         for _, achievementId in ipairs(achievementIds) do
-            local _, name = GetAchievementInfo(achievementId)
-            if name then
-                local link = GetAchievementLink(achievementId)
-                name = name:gsub("([()-])", "%%%1")
-                updateSourceText(name, link, sourceText)
-
-                break
+            local link = GetAchievementLink(achievementId)
+            if link then
+                if sourceType == 6 then
+                    updateSourceText(link, sourceText)
+                    break
+                else
+                    local _, name = GetAchievementInfo(achievementId)
+                    if name then
+                        name = name:gsub("([()-])", "%%%1")
+                        sourceText = sourceText:gsub(name, link)
+                        MountJournal.MountDisplay.InfoButton.Source:SetText(sourceText)
+                        break
+                    end
+                end
             end
         end
     elseif ADDON.DB.Source.Instance[spellId] and ADDON.DB.Source.Instance[spellId] ~= true then
@@ -33,9 +42,7 @@ local function replaceTextWithLinks()
         local difficultyId = ADDON.DB.Source.Instance[spellId][2]
         local name = EJ_GetEncounterInfo(encounterId)
         local link = C_EncounterJournal.GetEncounterJournalLink(1, encounterId, name, difficultyId)
-        if 0 == updateSourceText(":%s?|r%s?.+|n", ":|r "..link.."|n", sourceText) then
-            updateSourceText(":%s?|r%s?.+$", ":|r "..link, sourceText)
-        end
+        updateSourceText(link, sourceText)
     end
 end
 
