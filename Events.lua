7caaa5bd8d61095cb281f90674d99769f4160699
exochron <exochron@freenet.de>
2026-08-02 20:09:19 +0200

    fix(events): avoid secured UNIT_AURA event in 12.1

--- Events.lua
+++ Events.lua
@@ -1,12 +1,12 @@
 local _, ADDON = ...
 
-local activeMount, activeAuraId
+local activeMount
 
 function ADDON:IsPlayerMounted()
     return activeMount ~= nil
 end
 
-local function GetActiveMount()
+local function FetchActiveMountFromJournal()
     local numMounts = C_MountJournal.GetNumDisplayedMounts()
     for index = 1, numMounts do
         local _, _, _, isActive, _, _, _, _, _, _, isCollected, mountID = C_MountJournal.GetDisplayedMountInfo(index)
@@ -19,39 +19,26 @@ local function GetActiveMount()
     end
 end
 
-local function triggerEvents(unit, isOnLogin, mountId, auraInstanceID)
-    if mountId then
-        if unit == "player" then
-            if issecretvalue and issecretvalue(mountId) then
-                -- data from UNIT_AURA are now secret values since 12.0
-                -- therefore parse active mount again from different source
-                mountId = GetActiveMount()
-            end
-            activeMount = mountId
-            activeAuraId = auraInstanceID
-            ADDON.Events:TriggerEvent("OnMountUp", activeMount, isOnLogin)
-        elseif unit == "target" then
-            ADDON.Events:TriggerEvent("OnMountUpTarget", mountId)
-        end
-    end
-end
-
 local function checkAuras(auras)
     for _, aura in ipairs(auras) do
         if aura then
             local mountId = C_MountJournal.GetMountFromSpell(aura.spellId)
             if mountId then
-                return mountId, aura.auraInstanceID
+                return mountId
             end
         end
     end
 end
 
 function ADDON:ScanAuras(unit)
+    if InCombatLockdown() then
+        return
+    end
+
     local continuationToken, aura1, aura2, aura3, aura4, aura5
     repeat
         continuationToken, aura1, aura2, aura3, aura4, aura5 = C_UnitAuras.GetAuraSlots(unit, "HELPFUL|CANCELABLE", 5, continuationToken)
-        local mountId, auraInstanceID = checkAuras({
+        local mountId = checkAuras({
             aura1 and C_UnitAuras.GetAuraDataBySlot(unit, aura1),
             aura2 and C_UnitAuras.GetAuraDataBySlot(unit, aura2),
             aura3 and C_UnitAuras.GetAuraDataBySlot(unit, aura3),
@@ -59,48 +46,33 @@ function ADDON:ScanAuras(unit)
             aura5 and C_UnitAuras.GetAuraDataBySlot(unit, aura5),
         })
         if mountId then
-            return mountId, auraInstanceID
+            return mountId
         end
     until continuationToken == nil
 end
 
 ADDON.Events:RegisterCallback("OnLogin", function()
-    triggerEvents("player", true,  ADDON:ScanAuras("player"))
 
-    ADDON.Events:RegisterFrameEventAndCallback("UNIT_AURA", function(_, target, updateInfo)
-        if target == "player" or target == "target" then
-            if updateInfo.isFullUpdate then
-                triggerEvents(target, false,  ADDON:ScanAuras(target))
-            end
-            if updateInfo.addedAuras then
-                triggerEvents(target, false, checkAuras(updateInfo.addedAuras))
-            end
-            if target == "player" and updateInfo.removedAuraInstanceIDs and tContains(updateInfo.removedAuraInstanceIDs, activeAuraId) then
+    activeMount = FetchActiveMountFromJournal()
+    if activeMount then
+        ADDON.Events:TriggerEvent("OnMountUp", activeMount, true)
+    end
+
+    -- can be triggered by anyone surrounding you for mounting up and down
+    ADDON.Events:RegisterFrameEventAndCallback("COMPANION_UPDATE", function(_, type)
+        if type == "MOUNT" and activeMount then
+            local _,_,_, isActive = C_MountJournal.GetMountInfoByID(activeMount)
+            if not isActive then
                 local mountId = activeMount
                 activeMount = nil
-                activeAuraId = nil
                 ADDON.Events:TriggerEvent("OnMountDown", mountId)
             end
         end
-    end, 'external events')
-
-    ADDON.Events:RegisterFrameEventAndCallback("PLAYER_TARGET_CHANGED", function()
-        C_Timer.After(0, function()
-            triggerEvents("target", false,  ADDON:ScanAuras("target"))
-        end)
-    end, 'external events')
-
-    ADDON.Events:RegisterFrameEventAndCallback("NEW_MOUNT_ADDED", function(_, ...)
-        local param1, param2, param3 = ...
-        C_Timer.After(1, function() -- mount infos are not properly updated in current frame
-            ADDON:FilterMounts()
-            ADDON.Events:TriggerEvent("OnNewMount", param1, param2, param3)
-        end)
-    end, 'external events')
+    end)
 
     ADDON.Events:RegisterFrameEventAndCallback("UNIT_SPELLCAST_START", function(_, unit, _, spellId)
         if InCombatLockdown() -- no use case for now in combat
-           or (unit ~= "player" and unit ~= "target")
+            or (unit ~= "player" and unit ~= "target")
         then
             return
         end
@@ -113,6 +85,41 @@ ADDON.Events:RegisterCallback("OnLogin", function()
                 ADDON.Events:TriggerEvent("CastMountTarget", mountId)
             end
         end
+    end, 'external events')
+
+    ADDON.Events:RegisterFrameEventAndCallback("UNIT_SPELLCAST_SUCCEEDED", function(_, target, _, spellId)
+        if target == "player" then
+            local mountId = C_MountJournal.GetMountFromSpell(spellId)
+            if mountId then
+                local _,_,_, isActive = C_MountJournal.GetMountInfoByID(mountId)
+                if isActive then
+                    activeMount = mountId
+                    ADDON.Events:TriggerEvent("OnMountUp", activeMount, false)
+                end
+            end
+        elseif target == "target" then
+            local mountId = C_MountJournal.GetMountFromSpell(spellId)
+            if mountId then
+                ADDON.Events:TriggerEvent("OnMountUpTarget", mountId)
+            end
+        end
+    end, 'external events')
+
+    ADDON.Events:RegisterFrameEventAndCallback("PLAYER_TARGET_CHANGED", function()
+        C_Timer.After(0, function()
+            local mountId = ADDON:ScanAuras("target")
+            if mountId then
+                ADDON.Events:TriggerEvent("OnMountUpTarget", mountId)
+            end
+        end)
+    end, 'external events')
+
+    ADDON.Events:RegisterFrameEventAndCallback("NEW_MOUNT_ADDED", function(_, ...)
+        local param1, param2, param3 = ...
+        C_Timer.After(1, function() -- mount infos are not properly updated in current frame
+            ADDON:FilterMounts()
+            ADDON.Events:TriggerEvent("OnNewMount", param1, param2, param3)
+        end)
     end, 'external events')
 
     -- on unpacking mount
